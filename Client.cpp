9d3858c2d735186abf6b26d3d74f08c97cfdd180
Manmellon <comjagin@gmail.com>
2018-05-20 20:45:58 +0300

    Added SDL2 initialization

--- Client.cpp
+++ Client.cpp
@@ -1,7 +1,22 @@
+#include <SDL2/SDL.h>
 #include "Client.h"
 
 Client::Client()
 {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_GetCurrentDisplayMode(0, &displayMode);
+	
+	window = SDL_CreateWindow(
+		"The Day of Saggitarius III",
+		displayMode.w/4,
+		displayMode.h/4,
+		displayMode.w/2,
+		displayMode.h/2,
+		0
+		);
+	
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	
 	gameState = MENU;
 }
 
--- Client.h
+++ Client.h
@@ -2,6 +2,10 @@ class Client
 {
 	protected:
 		enum {MENU,GAME,PAUSE,OPTIONS,HELP,EXIT}gameState;
+		SDL_Window *window;
+		SDL_Renderer *renderer;
+		SDL_Event event;
+		SDL_DisplayMode displayMode;
 	public:
 		Client();
 		int MainLoop();
